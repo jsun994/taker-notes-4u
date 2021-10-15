e642c7cbc22b11f5ec852a8188a92257ba6be6c0
jsun <jaysun054@gmail.com>
2021-10-14 17:07:58 -0700

    routes

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const apiRoutes;
-const htmlRoutes;
-
 const express = require('express');
-const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.static('public'));
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
+app.use(express.static('Develop'));
 
+//routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
