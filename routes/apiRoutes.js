300b38ef88811522ff39303ed78bc1723903d076
jsun <jaysun054@gmail.com>
2021-10-14 17:15:52 -0700

    public folder

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
 const router = require('express').Router();
+
+router.get('/api/notes', (req, res) => {
+    
+});
+  
+router.post('/api/notes', (req, res) => {
+    
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('Develop'));
+app.use(express.static('public'));
 
 //routes
 app.use('/api', apiRoutes);
