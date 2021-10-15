66f217adcb300a5f3fc006e4a714da99dfaeffc8
jsun <jaysun054@gmail.com>
2021-10-14 20:10:21 -0700

    working routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,20 @@
-const fs = require('fs');
 const router = require('express').Router();
+const notes = require('../db/db.json');
+const fs = require('fs');
 
-router.get('/api/notes', (req, res) => {
-    
+router.get('/notes', (req, res) => {
+    res.json(notes);
 });
-  
-router.post('/api/notes', (req, res) => {
-    
+
+router.post('/notes', (req, res) => {
+    const save = req.body;
+    let results = notes
+    results.push(save);
+    fs.writeFile('db/db.json', JSON.stringify(results), (err) => {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+    res.json(results);
 });
 
 module.exports = router;
